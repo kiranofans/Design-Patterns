e5e73911e6f7aa68c84db9c281569445bdcd5696
Shu Zhang <shu@goopter.com>
2019-08-26 17:14:21 -0700

    Basic MVVM pattern app demo

---
+++ Basic_MVVM_Practice/app/src/main/java/project/android_projects/com/basic_mvvm_practice/MainActivity.java
@@ -0,0 +1,14 @@
+package project.android_projects.com.basic_mvvm_practice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
