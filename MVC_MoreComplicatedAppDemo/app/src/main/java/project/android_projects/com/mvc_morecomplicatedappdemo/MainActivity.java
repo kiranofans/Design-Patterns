e233e13912341b5cbb09b4614ad1c696ad2b2d12
Shu Zhang <shu@goopter.com>
2019-09-06 15:14:06 -0700

    Remove UI package

---
+++ MVC_MoreComplicatedAppDemo/app/src/main/java/project/android_projects/com/mvc_morecomplicatedappdemo/MainActivity.java
@@ -0,0 +1,14 @@
+package project.android_projects.com.mvc_morecomplicatedappdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
