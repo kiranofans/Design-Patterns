2fee8034755935c0c32cb885540cc15376fdb582
Shu Zhang <shu@goopter.com>
2019-09-06 15:31:54 -0700

    package arrangement

---
+++ MVP_MoreComplicatedAppDemo/app/src/main/java/project/android_projects/com/mvp_morecomplicatedappdemo/MainActivity.java
@@ -0,0 +1,14 @@
+package project.android_projects.com.mvp_morecomplicatedappdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
